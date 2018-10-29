e27356ba6d3ece547802f6c8d56bbffe3994f095
xraystyle <ben.levin@sohonet.com>
2018-10-29 10:31:27 -0700

    Clean up excludes list for readability, remove unnecesary whitespace.

--- s3sync.rb
+++ s3sync.rb
@@ -18,77 +18,72 @@
 # Placed here so they only need to be typed out once and can be modified in a single place.
 # Feel free to add to or subtract from the list as necessary. This list of excludes skips
 # most of the unnecessary files you'd find on a NAS in a typical office. Adjust as needed.
-@excludes = "--exclude '.DS_Store' --exclude '*/.DS_Store' --exclude 'Thumbs.db' --exclude '*/Thumbs.db' --exclude '.apdisk' --exclude '*/.apdisk' --exclude '.AppleDB/*' --exclude '*/.AppleDB/*' --exclude '.AppleDesktop/*' --exclude '*/.AppleDesktop/*' --exclude '.AppleDouble/*' --exclude '*/.AppleDouble/*' --exclude '.TemporaryItems/*' --exclude '*/.TemporaryItems/*' --exclude '._*' --exclude '*/._*' --exclude '~$*' --exclude '*/~$*'"
+excludes_array = [
+	"--exclude '.DS_Store'",
+	"--exclude '*/.DS_Store'",
+	"--exclude 'Thumbs.db'",
+	"--exclude '*/Thumbs.db'",
+	"--exclude '.apdisk'",
+	"--exclude '*/.apdisk'",
+	"--exclude '.AppleDB/*'",
+	"--exclude '*/.AppleDB/*'",
+	"--exclude '.AppleDesktop/*'",
+	"--exclude '*/.AppleDesktop/*'",
+	"--exclude '.AppleDouble/*'",
+	"--exclude '*/.AppleDouble/*'",
+	"--exclude '.TemporaryItems/*'",
+	"--exclude '*/.TemporaryItems/*'",
+	"--exclude '._*'",
+	"--exclude '*/._*'",
+	"--exclude '~$*'",
+	"--exclude '*/~$*'"
+]
 
-
-
+# Make a space-separated string out of the above array.
+@excludes = excludes_array.join(' ')
 
 
 # Method definitions --------------------------------------------------------------------------------------------------
 
 
-# sync individual directories passed to this method to the readynas s3 bucket.
+# sync individual directories passed to this method to the s3 bucket.
 def sync_dir_to_s3(local_dir)
-
 	# escape spaces in paths
 	local_dir = local_dir.gsub(' ', '\ ')
-
 	s3path = "s3://<s3bucket>" + local_dir 
 
 	# use 'cp' if it's a file. 'sync' only works with directories.
 	if File.file?(local_dir)
-
 		s3command = "aws s3 cp " + local_dir + " " + s3path
-
 	else
-
 		s3command = "aws s3 sync " + local_dir + " " + s3path + " " + @excludes + " --delete"
-
 	end
 
 	begin		
-		
 		s3_out = `#{s3command}`.split("\n") # Split s3command on newlines, so each can be output as it's own line.
-
 		# if the output of the command is a blank string, it's because 'sync' found nothing to sync.
 		if s3_out.count == 0
-
 			`/bin/echo '#{Time.now}: Skipping #{local_dir}, no changes necessary.' >> /path/to/log/s3sync.log`
-
 		else
-
 			`/bin/echo #{Time.now}: >> /path/to/log/s3sync.log`
-
 			# aws s3 sync gives constant updates to stdout in the form of "completed X of Y parts with Z file(s) remaining".
 			# This output can become large enough to choke `echo` when outputting to the logs. All we want is the end of the 
 			# string, the part that tells us what file was either uploaded or deleted. e.g. "delete: s3://<s3bucket>/somefolder/somefile.jpg"
 			# snatch it with regex and output that part only.
 			s3_out.each do |line|
-
 				clean_line = line.match(/((?:upload|delete):+.*$)/)
-
 				# if clean_line is nil, no match was found.
 				next if clean_line.nil?
-
 				# echo the first match group to the log.
 				`/bin/echo '    #{clean_line[1]}' >> /path/to/log/s3sync.log`
-
 			end
-
 		end
 
 	rescue Exception => e
-
 		`/bin/echo '#{Time.now}: Script error: #{e}' >> /path/to/log/s3sync.log`
-
 	end
-	
-
 
 end
-
-
-
 
 
 
